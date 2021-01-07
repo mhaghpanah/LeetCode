7535b3e61a0c25d0af612248de02dffc2f138e61
mhaghpanah <40441978+mhaghpanah@users.noreply.github.com>
2021-01-07 12:50:21 -0600

    Update 222-CountCompleteTreeNodes.java

--- 222-CountCompleteTreeNodes.java
+++ 222-CountCompleteTreeNodes.java
@@ -14,43 +14,47 @@
  * }
  */
 class Solution {
-    
-    public int height(TreeNode node) {
-        int h = 0;
-        while (node != null) {
-            node = node.left;
-            h++;
-        }
-        return h;
+
+  public int height(TreeNode node) {
+    int h = 0;
+    while (node != null) {
+      node = node.left;
+      h++;
     }
-    
-    public boolean isExists(TreeNode node, int h, int idx) {
-        for (int i = h - 2; i >= 0; i--) {
-            if ((idx & (1 << i)) != 0) {
-                node = node.right;
-            } else {
-                node = node.left;
-            }
-        }
-        return node != null;
+    return h;
+  }
+
+  public boolean isExists(TreeNode node, int h, int idx) {
+    for (int i = h - 2; i >= 0; i--) {
+      if ((idx & (1 << i)) != 0) {
+        node = node.right;
+      } else {
+        node = node.left;
+      }
     }
-    
-    public int countNodes(TreeNode root) {
-        if (root == null) return 0;
-        
-        int h = height(root);
-        int lo = 1 << (h - 1);
-        int hi = (1 << h) - 1;
-        int ans = lo;
-        
-        while (lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
-            if (isExists(root, h, mid)) {
-                ans = mid;
-                lo = mid + 1;
-            } else hi = mid - 1;
-        }
-        
-        return ans;
+    return node != null;
+  }
+
+  public int countNodes(TreeNode root) {
+    if (root == null) {
+      return 0;
     }
+
+    int h = height(root);
+    int lo = 1 << (h - 1);
+    int hi = (1 << h) - 1;
+    int ans = lo;
+
+    while (lo <= hi) {
+      int mid = lo + (hi - lo) / 2;
+      if (isExists(root, h, mid)) {
+        ans = mid;
+        lo = mid + 1;
+      } else {
+        hi = mid - 1;
+      }
+    }
+
+    return ans;
+  }
 }
