9087e9b6d2af4bd7f9954de5cbedc8c96faadb53
mhaghpanah <40441978+mhaghpanah@users.noreply.github.com>
2021-02-26 12:57:59 -0600

    Create 78-Subsets.java

---
+++ 78-Subsets.java
@@ -0,0 +1,18 @@
+class Solution {
+
+  public List<List<Integer>> subsets(int[] nums) {
+    int n = nums.length;
+    List<List<Integer>> ans = new ArrayList<>();
+    for (int i = 0; i < (1 << n); i++) {
+      List<Integer> list = new ArrayList<>();
+      for (int j = 0; j < n; j++) {
+        if (((i >> j) & 1) != 0) {
+          list.add(nums[j]);
+        }
+      }
+      ans.add(list);
+    }
+
+    return ans;
+  }
+}
