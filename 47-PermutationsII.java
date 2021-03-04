f73b78e5761f9acafe99a40ec9a33be2d52a945a
mhaghpanah <40441978+mhaghpanah@users.noreply.github.com>
2021-03-03 20:02:48 -0600

    Create 47-PermutationsII.java

---
+++ 47-PermutationsII.java
@@ -0,0 +1,50 @@
+class Solution {
+
+  public List<Integer> toList(int[] nums) {
+    List<Integer> ans = new ArrayList<>();
+    for (int a : nums) {
+      ans.add(a);
+    }
+    return ans;
+  }
+
+  public void swap(int[] nums, int i, int j) {
+    int t = nums[i];
+    nums[i] = nums[j];
+    nums[j] = t;
+  }
+
+  public void rev(int[] nums, int lo) {
+    int hi = nums.length - 1;
+    while (lo < hi) {
+      swap(nums, lo++, hi--);
+    }
+  }
+
+  public boolean next(int[] nums) {
+    int n = nums.length;
+    int i = n - 2;
+    while (i >= 0 && nums[i] >= nums[i + 1]) {
+      i--;
+    }
+    if (i < 0) {
+      return false;
+    }
+    int j = n - 1;
+    while (nums[i] >= nums[j]) {
+      j--;
+    }
+    swap(nums, i, j);
+    rev(nums, i + 1);
+    return true;
+  }
+
+  public List<List<Integer>> permuteUnique(int[] nums) {
+    Arrays.sort(nums);
+    List<List<Integer>> ans = new ArrayList<>();
+    do {
+      ans.add(toList(nums));
+    } while (next(nums));
+    return ans;
+  }
+}
