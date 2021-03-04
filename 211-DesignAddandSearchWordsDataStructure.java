feb14f9be4bfecfabb88a9f6a3a75d67b5b9c58a
mhaghpanah <40441978+mhaghpanah@users.noreply.github.com>
2021-03-04 12:06:19 -0600

    Create 211-DesignAddandSearchWordsDataStructure.java

---
+++ 211-DesignAddandSearchWordsDataStructure.java
@@ -0,0 +1,70 @@
+class WordDictionary {
+
+  TrieNode root;
+
+  /**
+   * Initialize your data structure here.
+   */
+  public WordDictionary() {
+    root = new TrieNode();
+  }
+
+  /**
+   * Adds a word into the data structure.
+   */
+  public void addWord(String word) {
+    TrieNode h = root;
+    for (char ch : word.toCharArray()) {
+      h = h.children.computeIfAbsent(ch, k -> new TrieNode());
+    }
+    h.isWord = true;
+  }
+
+  /**
+   * Returns if the word is in the data structure. A word could contain the dot character '.' to
+   * represent any one letter.
+   */
+  public boolean search(String word) {
+    return search(root, word, 0);
+  }
+
+  public boolean search(TrieNode h, String word, int i) {
+    int n = word.length();
+    for (; i < n; i++) {
+      char ch = word.charAt(i);
+      if (ch == '.') {
+        for (TrieNode node : h.children.values()) {
+          if (search(node, word, i + 1)) {
+            return true;
+          }
+        }
+        return false;
+      } else {
+        if (!h.children.containsKey(word.charAt(i))) {
+          return false;
+        }
+        h = h.children.get(ch);
+      }
+
+    }
+    return h.isWord;
+  }
+
+  class TrieNode {
+
+    boolean isWord;
+    Map<Character, TrieNode> children;
+
+    public TrieNode() {
+      children = new HashMap<>();
+    }
+  }
+}
+
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary obj = new WordDictionary();
+ * obj.addWord(word);
+ * boolean param_2 = obj.search(word);
+ */
